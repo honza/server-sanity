04b89da4f6cf8ea8addc83fedd39f49136251e0c
Honza Pokorny <me@honza.ca>
2018-01-26 08:31:48 -0400

    Add post install instructions

--- run.sh
+++ run.sh
@@ -5,3 +5,17 @@ git clone https://github.com/honza/server-sanity
 cd server-sanity
 pip install ansible
 ansible-playbook server-sanity.yaml
+
+cat <<EOF
+=============================
+server sanity setup complete!
+=============================
+
+Here is what's left to do:
+
+1.  Log out
+2.  Log back in with mosh
+3.  Start tmux
+4.  Profit!
+
+EOF
